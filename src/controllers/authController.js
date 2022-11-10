5c305193b672f89a3a756f119dfed86770baa330
vnniciusg109 <90412694+vnniciusg109@users.noreply.github.com>
2022-11-09 21:48:57 -0300

    asd

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
-const User = require('../models/User');
+const User = require('../models/user');
 const jwt = require('jsonwebtoken');
 
 const authConfig = require('../config/auth.json');
