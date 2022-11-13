5cb0cd2e7548bd8ab9d11b0f6c8f1abdb58b2723
vnniciusg109 <90412694+vnniciusg109@users.noreply.github.com>
2022-11-13 15:26:26 -0300

    asdasd

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ var port = process.env.PORT || 8080;
 
 
 const app = express()
+const cors = require('cors');
+app.use(cors());
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
