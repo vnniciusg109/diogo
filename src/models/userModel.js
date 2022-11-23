247dd797d9d97848950627a25151f8fd45237cf8
vnniciusg109 <90412694+vnniciusg109@users.noreply.github.com>
2022-11-23 14:13:20 -0300

    alteraçoes

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,11 +1,7 @@
 const mongoose = require ('../database');
 const bcrypt = require('bcryptjs')
-const validator = require('validator')
 const jwt = require('jsonwebtoken')
 require('dotenv').config()
-
-
-const JWT_SECRET = process.env.JWT_SECRET
 
 
 const UserTestSchema = new mongoose.Schema({
@@ -37,73 +33,17 @@ const UserTestSchema = new mongoose.Schema({
         unique: true,
         required: true,
         lowercase: true,
-        validate(value){
-            if(!validator.isEmail(value)){
-                throw new Error('Email is invalid')
-            }
-        }
     },
     password: {
         type: String,
         required: true,
         minLenght: 7,
         trim:true,
-        validate(value){
-            if(value.toLowerCase().includes('password')){
-                throw new Error('password mus\'t contain password')
-            }
-        }
-    },
 
-    tokens :[{
-        token:{
-            type:String,
-            required:true
-        }
-    }]
 
+    }
 },{timestamps:true}
 )
-
-
-//Gerar Token
-UserTestSchema.methods.generateAuthToken = async function(){
-    const user= this
-    const token = jwt.sign({_id:user._id.toString()},process.env.JWT_SECRET)
-    user.tokens = user.tokens.concat({token})
-    await user.save()
-    return token
-}
-
-//Login 
-UserTestSchema.statics.findByCredentials = async(email,password) =>{
-    const user = await UserTest.findOne({email})
-    if(!user){
-        throw new Error("Unable to log in")
-    }
-
-    const isMatch = await bcrypt.compare(password,user.password)
-    console.log(isMatch)
-
-    if(!isMatch){
-        throw new Error("Unable to log in")
-    }
-
-    return user
-}
-
-
-//Hash Senha
-UserTestSchema.pre('save',async function(next){
-    const user = this
-    if(user.isModified('password')){
-        user.password = await bcrypt.hash(user.password,8)
-    }
-
-    next()
-})
-
-
 
 const UserTest = mongoose.model('UserTest', UserTestSchema)
 
