a12c8661cb44662570c1b63b6c5df669881bd469
vnniciusg109 <90412694+vnniciusg109@users.noreply.github.com>
2022-11-23 11:01:01 -0300

    teste

--- src/controllers/ticketController.js
+++ src/controllers/ticketController.js
@@ -17,10 +17,19 @@ const getTicket = ((req, res) => {
 
 //Criar Ingresso
 
-const createTicket = ((req, res) => {
-  Ticket.create({...req.body,ownerTicket : req.user._id})
-      .then(result => res.status(200).json({ result }))
-      .catch((error) => res.status(500).json({msg:  error }))
+const createTicket = (async(req, res) => {
+  try{
+    const newTicket = new Ticket({
+      ...req.body,
+      ownerTicket : req.user._id       
+    })
+    await newTicket.save()
+    res.status(201).send(newTicket)
+  }catch(error){
+    console.log({error})
+    res.status(400).send({message:"aqui"})
+  }
+
 })
 
 
