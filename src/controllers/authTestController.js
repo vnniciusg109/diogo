e1a9a71e3865ad1c7de32ac9047be43027f96629
vnniciusg109 <90412694+vnniciusg109@users.noreply.github.com>
2022-11-23 14:23:31 -0300

    Update authTestController.js

--- src/controllers/authTestController.js
+++ src/controllers/authTestController.js
@@ -53,6 +53,7 @@ const Login = (req,res) =>{
             id: user._id,
             username:user.username,
             email : user.email,
+            token :req.session.token
         })
     })
 
