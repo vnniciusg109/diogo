be7bf4c7298aba2645e8a7e1e7a49f5507ea6086
binudo <diogomf358@gmail.com>
2022-11-30 21:05:17 -0300

    Update index.js

--- index.js
+++ index.js
@@ -21,9 +21,7 @@ var port = process.env.PORT || 8080;
 const app = express()
 
 app.use((req, res, next) => {
-	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
     res.header("Access-Control-Allow-Origin", "*");
-	//Quais são os métodos que a conexão pode realizar na API
     res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
     app.use(cors());
     next();
