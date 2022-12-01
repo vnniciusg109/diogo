5a8f253ac8e51885f78e408da6b8e036e572387e
vnniciusg109 <90412694+vnniciusg109@users.noreply.github.com>
2022-12-01 09:27:25 -0300

    Update authenticationController.js

--- src/controllers/authenticationController.js
+++ src/controllers/authenticationController.js
@@ -1,6 +1,7 @@
 const User = require('../models/userModel');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
+const{StatusCodes} = require('http-status-codes')
 
 require('dotenv').config()
 
